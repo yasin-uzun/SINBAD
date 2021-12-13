4ab8ba551586f75796227b83619eea7a11b230fc
yasin-uzun <yasin.delco@gmail.com>
2021-12-13 14:25:28 -0500

    Alpha

--- R/Main.R
+++ R/Main.R
@@ -1199,236 +1199,40 @@ wrap_remove_temporary_files <- function(sinbad_object)
 
 }
 
-
-process_sample_wrapper <- function(raw_fastq_dir, demux_index_file, working_dir, sample_name) 
+process_sample_wrapper <- function(sinbad_object)
 {
 
-	sinbad_object = construct_sinbad_object(working_dir = working_dir,
-		                                raw_fastq_dir = raw_fastq_dir,
-		                                demux_index_file = demux_index_file,
-		                                sample_name = sample_name)
 
-	flag_r2_index_embedded_in_r1_reads  = FALSE
+  par(mfrow = c(2,2))
 
-	if(exists('sequencing_type') & exists('is_r2_index_embedded_in_r1_reads') )
-	{
-	  if(sequencing_type == 'paired' & is_r2_index_embedded_in_r1_reads)
-	  {
-	    flag_r2_index_embedded_in_r1_reads  = TRUE
-	    
-	  }
-	}
+  sinbad_object = wrap_demux_fastq_files(sinbad_object)
 
+  sinbad_object = wrap_demux_stats(sinbad_object)
 
-	if(flag_r2_index_embedded_in_r1_reads )
-	{
-	  
-	  get_r2_indeces_from_r1(r1_fastq_dir = raw_fastq_dir,
-		                 r2_input_fastq_dir = raw_fastq_dir,
-		                 r2_output_fastq_dir = sinbad_object$r2_meta_fastq_dir,
-		                 sample_name = sample_name)
-	  
-	}
+  sinbad_object = wrap_trim_fastq_files(sinbad_object)
 
-	#Demux
-	sinbad_object = wrap_demux_fastq_files(sinbad_object, flag_r2_index_embedded_in_r1_reads)
-	sinbad_object = wrap_demux_stats(sinbad_object)
+  sinbad_object = wrap_trim_stats(sinbad_object)
 
-	rownames(sinbad_object$df_demux_reports) = gsub('_merged', '', rownames(sinbad_object$df_demux_reports) )
+  sinbad_object = wrap_align_sample(sinbad_object)
 
-	sinbad_object_file = paste0(working_dir, sample_name, '/objects/sinbad_object.03.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
+  sinbad_object = wrap_generate_alignment_stats(sinbad_object)
 
-	print('Demux done')
-	print(sinbad_object)
+  sinbad_object = wrap_call_methylation_sites(sinbad_object)
 
-	#Trim
+  sinbad_object = wrap_generate_methylation_stats(sinbad_object)
 
-	sinbad_object_file = paste0(working_dir, sample_name, '/objects/sinbad_object.03.rds')
-	sinbad_object = readRDS(sinbad_object_file)
+  sinbad_object = wrap_read_regions(sinbad_object)
 
-	sinbad_object$df_demux_reports
+  sinbad_object = wrap_quantify_regions(sinbad_object)
 
-	sinbad_object = wrap_trim_fastq_files(sinbad_object)
-	print('Trimming done')
+  sinbad_object = wrap_dim_red(sinbad_object)
 
-	sinbad_object = wrap_trim_stats(sinbad_object)
+  sinbad_object = wrap_dmr_analysis(sinbad_object)
 
-	sinbad_object_file = paste0(working_dir, sample_name, '/objects/sinbad_object.05.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
 
-	#sinbad_object_001 = sinbad_object
-
-	wrap_plot_preprocessing_stats(sinbad_object)
-
-
-	sinbad_object_file = paste0(working_dir, sample_name, '/objects/sinbad_object.05.rds')
-	sinbad_object = readRDS(sinbad_object_file)
-	print(sinbad_object)
-
-
-	#Align
-	sinbad_object = wrap_align_sample(sinbad_object)
-	sinbad_object_file = paste0(working_dir,sample_name, '/objects/sinbad_object.06.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	print(sinbad_object)
-
-
-	#Alignment stats
-	sinbad_object = wrap_generate_alignment_stats(sinbad_object)
-	sinbad_object_file = paste0(working_dir, sample_name, '/objects/sinbad_object.07.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	print(sinbad_object)
-	print(head(sinbad_object$df_alignment_stats))
-	print(tail(sinbad_object$df_alignment_stats))
-
-
-
-	#Merge bam files
-	wrap_merge_r1_and_r2_bam(sinbad_object)
-
-	#Coverage
-	sinbad_object = wrap_compute_coverage_rates(sinbad_object)
-	sinbad_object_file = paste0(working_dir, sample_name, '/objects/sinbad_object.08.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	print(sinbad_object)
-
-
-
-
-
-
-
-	minimum_filtered_read_count = 2000000
-	alignment_rate_threshold = 40
-
-	#Plot alignment QC
-	sinbad_object = wrap_plot_alignment_stats(sinbad_object)
-
-	#Call methylation 
-	sinbad_object = wrap_call_methylation_sites(sinbad_object)
-
-	sinbad_object_file = paste0(working_dir,sample_name, '/objects/sinbad_object.09.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	print(sinbad_object)
-
-
-
-	#Methylation stats
-
-	sinbad_object = wrap_generate_methylation_stats(sinbad_object)
-
-	sinbad_object_file = paste0(working_dir,sample_name, '/objects/sinbad_object.10.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	print(head(sinbad_object$df_alignment_stats))
-
-
-	options('bitmapType')
-	options(bitmapType='cairo')
-
-	sinbad_object = wrap_plot_met_stats(sinbad_object)
-	print(sinbad_object$plot_dir)
-
-	options(bitmapType = "Xlib")
-
-	##########Read annotation matrices##########
-
-	annot_format_file =  paste0(annot_dir, "/annot_file_format.txt")
-
-	annot_file = paste0(annot_dir, '/hg38/', 'regions.Bins_100Kb.bed')
-	sinbad_object = wrap_read_annot(sinbad_object, annot_file = annot_file, annot_format_file = annot_format_file, annot_name = 'Bins_100Kb')
-
-	annot_file = paste0(annot_dir, '/hg38/', 'regions.Bins_10Kb.bed')
-	sinbad_object = wrap_read_annot(sinbad_object, annot_file = annot_file, annot_format_file = annot_format_file, annot_name = 'Bins_10Kb')
-
-	annot_file = paste0(annot_dir, '/hg38/', 'regions.all_genes.bed')
-	sinbad_object = wrap_read_annot(sinbad_object, annot_file = annot_file, annot_format_file = annot_format_file, annot_name = 'Gene_Body')
-
-	annot_file = paste0(annot_dir, '/hg38/', 'regions.all_genes_plus_2Kb.bed')
-	sinbad_object = wrap_read_annot(sinbad_object, annot_file = annot_file, annot_format_file = annot_format_file, annot_name = 'Gene_Body-+2Kb')
-
-	annot_file = paste0(annot_dir, '/hg38/', 'regions.TSS_up2Kb_down2Kb.bed')
-	sinbad_object = wrap_read_annot(sinbad_object, annot_file = annot_file, annot_format_file = annot_format_file, annot_name = 'TSS-+2Kb')
-
-	annot_file = paste0(annot_dir, '/hg38/', 'regions.TSS_up2Kb_down500bp.bed')
-	sinbad_object = wrap_read_annot(sinbad_object, annot_file = annot_file, annot_format_file = annot_format_file, annot_name = 'TSS-2Kb+500bp')
-
-
-
-	sinbad_object_file = paste0(working_dir, sample_name,'/objects/sinbad_object.11.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	#print(sinbad_object)
-
-
-	##########Quantify methylation matrices##########
-
-	annot_names = names(sinbad_object$annot_list)
-	print(annot_names)
-	 
-	for(annot_name in annot_names)
-	{
-	  print(annot_name)
-	  sinbad_object = wrap_quantify_regions(sinbad_object, annot_name = annot_name)
-	}
-
-	print(names(sinbad_object$met_matrices))
-
-	sinbad_object_file = paste0(working_dir, sample_name,'/objects/sinbad_object.12.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	print(sinbad_object)
-
-
-	##########Impute NA values##########
-
-
-	for(annot_name in annot_names)
-	{
-	  print(annot_name)
-	  sinbad_object = wrap_impute_nas(sinbad_object, annot_name)
-	}
-
-	sinbad_object_file = paste0(working_dir,sample_name, '/objects/sinbad_object.13.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-
-
-	##########Run dimensionality reduction##########
-
-	annot_name = "Bins_100Kb"
-	for(annot_name in annot_names)
-	{
-	  print(annot_name)
-	  sinbad_object = wrap_dim_red(sinbad_object, annot_name)
-	}
-
-	sinbad_object_file = paste0(working_dir,sample_name, '/objects/sinbad_object.13.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-
-
-	##########Run differential methylation##########
-
-
-	sinbad_object = wrap_dmr_analysis(sinbad_object)
-
-	sinbad_object_file = paste0(working_dir,sample_name, '/objects/sinbad_object.14.rds')
-	saveRDS(sinbad_object,   file = sinbad_object_file)
-	sinbad_object = readRDS(sinbad_object_file)
-	print(sinbad_object)
-
+  return(sinbad_object)
 
 
 }
-
-
-
-
 
 
---
+++ R/Shiny_GUI.R
@@ -0,0 +1,184 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("SINBAD"),
+
+  sidebarLayout(
+    sidebarPanel(
+      #helpText("Parameter settings"),
+
+      tabsetPanel(
+        tabPanel("Settings",
+
+        br(),
+
+        textInput("sample_name", "Sample name: ",
+                  value = "Sample"),
+
+        actionButton("browse_config_dir", "Config dir", width = '100px', style='padding:4px; background-color:#C0C0C0; border-color: #696969'),
+        actionButton("browse_demux_index_file", "Demux file", width = '100px', style='padding:4px; background-color:#C0C0C0; border-color: #696969'),
+        br(),
+        br(),
+
+        actionButton("browse_fastq_dir", "Read dir", width = '100px', style='padding:4px; background-color:#C0C0C0; border-color: #696969'),
+        actionButton("browse_working_dir", "Output dir", width = '100px', style='padding:4px; background-color:#C0C0C0; border-color: #696969'),
+        br(),
+        br(),
+
+        actionButton("save_sinbad_object", "Save results", width = '100px', style='padding:4px; background-color:#C0C0C0; border-color: #696969'),
+        actionButton("load_sinbad_object", "Load results", width = '100px', style='padding:4px; background-color:#C0C0C0; border-color: #696969'),
+
+        br(),
+        br(),
+
+        selectInput("sequencing_type",
+                    label = "Sequencing type",
+                    choices = c("paired",
+                                "single"),
+                    selected = "paired"),
+
+        numericInput("demux_index_length", "Demultiplexing index length: ",
+                  value = "6"),
+
+        radioButtons("is_r2_index_embedded_in_r1_reads", "Is right index embedded in left reads?",
+                     choices = list("Yes" = T, "No" = F), selected = T, inline = T),
+
+
+        numericInput("num_cores", "Number of cores: ", value = "16"),
+
+
+
+      #br(),
+      #br(),
+
+        ),
+      tabPanel("Execute",
+      br(),
+
+      actionButton("btn_preprocess", "Preprocess", class = "btn-warning", width = '100px', style='padding:4px'  ) ,
+      actionButton("btn_pp_stats", "Plot PP Stats", class = "btn-warning", width = '100px', style='padding:4px' ) ,
+
+      br(),
+      br(),
+
+      numericInput("mapq_threshold", "Mapping quality threshold: ", value = "10"),
+
+      sliderInput("alignment_rate_threshold", "Minimum alignment rate:",
+                  min = 0, max = 100,
+                  value = 20),
+
+      numericInput("minimum_filtered_read_count", "Minimum read count for cell: ", value = "200000"),
+
+      actionButton("btn_align", "Align", class = "btn-warning", width = '100px', style='padding:4px'),
+      actionButton("btn_align_stats", "Plot Alignment", class = "btn-warning", width = '100px', style='padding:4px' ) ,
+
+      br(),
+      br(),
+
+      actionButton("btn_call_met", "Call Met.", class = "btn-warning", width = '100px', style='padding:4px'),
+      actionButton("btn_met_stats", "Plot Met. Stats", class = "btn-warning", width = '100px', style='padding:4px'),
+
+      br(),
+      br(),
+
+      numericInput("min_call_count_threshold", "Minimum call count for region: ", value = "10"),
+      numericInput("max_ratio_of_na_cells", "Maximum ratio of missing cells for region: ", value = "0.25"),
+
+
+      #actionButton("btn_align_stats", "Plot Alignment Stats", class = "btn-warning" ) ,
+
+      #br(),
+      #br(),
+      actionButton("btn_Quantify", "Quantify", class = "btn-warning", width = '100px', style='padding:4px'),
+
+      actionButton("btn_DMR", "DMR", class = "btn-warning", width = '100px', style='padding:4px')
+
+      )
+
+      )
+
+    ),
+
+    mainPanel(
+      #textOutput("selected_var"),
+      #textOutput("min_max")
+      mainPanel(
+        # Use imageOutput to place the image on the page
+        imageOutput("myImage")
+      )
+    )
+  )
+)
+
+
+
+server <- function(input, output) {
+
+  output$selected_var <- renderText({
+    paste("You have selected", input$sequencing_type)
+  })
+
+  output$min_max <- renderText({
+    paste("You have chosen minimum alignment rate",
+          input$alignment_rate_threshold[1])
+  })
+
+
+  observeEvent(input$btn_align_stats, {
+
+    outfile = paste0(sinbad_object$plot_dir,  "/QC/Preprocessing_statistics.png")
+    error_msg = paste(outfile, 'cannot be found.')
+    output$myImage <- renderImage({
+
+      # Return a list containing the filename
+      list(src = outfile,
+           contentType = 'image/png',
+           width = 800,
+           height = 600,
+           alt = error_msg)
+    }, deleteFile = FALSE)
+  })
+
+
+
+
+  observeEvent(input$btn_pp_stats, {
+
+    outfile = paste0(sinbad_object$plot_dir,  "/QC/Alignment_statistics.png")
+    error_msg = paste(outfile, 'cannot be found.')
+    output$myImage <- renderImage({
+
+      # Return a list containing the filename
+      list(src = outfile,
+           contentType = 'image/png',
+           width = 800,
+           height = 600,
+           alt = error_msg)
+    }, deleteFile = FALSE)
+
+  })
+
+
+  observeEvent(input$btn_met_stats, {
+
+    outfile = paste0(sinbad_object$plot_dir,  "/QC/Met_call_statistics.png")
+    error_msg = paste(outfile, 'cannot be found.')
+    output$myImage <- renderImage({
+
+      # Return a list containing the filename
+      list(src = outfile,
+           contentType = 'image/png',
+           width = 800,
+           height = 600,
+           alt = error_msg)
+    }, deleteFile = FALSE)
+  })
+
+
+
+
+
+}
+
+shinyApp(ui, server)
+
