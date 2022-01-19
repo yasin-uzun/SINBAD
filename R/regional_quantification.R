a307d8ad8b445e9651d6393681f5e3b8aa763203
Shane Drabing <shane.drabing@gmail.com>
2022-01-19 09:19:32 -0700

    unlist lapply call

--- R/regional_quantification.R
+++ R/regional_quantification.R
@@ -177,9 +177,7 @@ compute_call_count_matrices <- function(  df_region,
     library(data.table)
     print("??? load data.table")
 
-    print(quant_cols)
-
-    dt_aggr <- dt_inter[, lapply(.SD, sum), by = .(region_name), .SDcols = unlist(quant_cols)]
+    dt_aggr <- dt_inter[, unlist(lapply(.SD, sum)), by = .(region_name), .SDcols = quant_cols ]
     print("??? subset data.table")
 
     length(unique(dt_aggr$region_name))
