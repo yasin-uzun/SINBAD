2d7025b8dd0e5820941ffae23c76ce4969b6450d
Shane Drabing <shane.drabing@gmail.com>
2022-01-19 09:03:48 -0700

    debug messages

--- R/regional_quantification.R
+++ R/regional_quantification.R
@@ -175,8 +175,11 @@ compute_call_count_matrices <- function(  df_region,
     quant_cols = c('met', 'demet')
 
     library(data.table)
+    print("??? load data.table")
 
     dt_aggr <- dt_inter[, lapply(.SD, sum), by = .(region_name), .SDcols = quant_cols  ]
+    print("??? subset data.table")
+  
     length(unique(dt_aggr$region_name))
     head(dt_aggr)
     dim(dt_aggr)
