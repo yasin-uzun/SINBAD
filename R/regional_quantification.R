424c16b5939b7d3aa84809727a01dbef03fc85fd
Shane Drabing <shane.drabing@gmail.com>
2022-01-19 09:17:18 -0700

    try unlist

--- R/regional_quantification.R
+++ R/regional_quantification.R
@@ -179,7 +179,7 @@ compute_call_count_matrices <- function(  df_region,
 
     print(quant_cols)
 
-    dt_aggr <- dt_inter[, lapply(.SD, sum), by = .(region_name), .SDcols = quant_cols  ]
+    dt_aggr <- dt_inter[, lapply(.SD, sum), by = .(region_name), .SDcols = unlist(quant_cols)]
     print("??? subset data.table")
 
     length(unique(dt_aggr$region_name))
