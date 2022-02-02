241b890ceac508fdf6a5f2a276d4a1c5bf1166be
Shane Drabing <shane.drabing@gmail.com>
2022-02-02 11:19:33 -0700

    oops

--- R/regional_quantification.R
+++ R/regional_quantification.R
@@ -175,6 +175,11 @@ compute_call_count_matrices <- function(  df_region,
 
     # library(data.table)
     print("!!! begin aggr")
+  
+    dt_inter <- data.frame(
+      region_name = c("A", "A", "B"),
+      met = 1:3, demet = 1:3
+    )
 
     keys <- sort(unique(dt_inter$region_name))
     dt_aggr <- rep(list(c(met = 0, demet = 0)), length(keys))
@@ -192,6 +197,9 @@ compute_call_count_matrices <- function(  df_region,
       dt_aggr[[region_name]]["met"] <- dt_aggr[[region_name]]["met"] + met
       dt_aggr[[region_name]]["demet"] <- dt_aggr[[region_name]]["demet"] + demet
     }
+
+    dt_aggr <- as.data.frame(do.call(rbind, dt_aggr))
+    dt_aggr$region_name <- rownames(dt_aggr)
 
     # lst <- tapply(dt_inter$region_name, 1:length(dt_inter$region_name), c)
     # for (k in names(lst)) {
