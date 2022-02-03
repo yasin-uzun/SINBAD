c1c4a54019521bba01530148ce44241be6015189
Shane Drabing <shane.drabing@gmail.com>
2022-02-03 10:28:09 -0700

    try data.table

--- R/regional_quantification.R
+++ R/regional_quantification.R
@@ -1,5 +1,5 @@
 # library(dplyr)
-# library(data.table::data.table)
+library(data.table)
 # library(GenomicRanges)
 
 read_region_annot <- function(region_annot_file, format_file, type_filtering =  'none')
@@ -173,33 +173,32 @@ compute_call_count_matrices <- function(  df_region,
 
     quant_cols = c('met', 'demet')
 
-    # library(data.table)
     print("!!! begin aggr")
   
-    # dt_inter <- data.frame(
+    # dt_inter <- data.table(
     #   region_name = c("A", "A", "B"),
     #   met = 1:3, demet = 1:3
     # )
 
-    keys <- sort(unique(dt_inter$region_name))
-    dt_aggr <- rep(list(c(met = 0, demet = 0)), length(keys))
-    dt_aggr <- setNames(dt_aggr, keys)
+    # keys <- sort(unique(dt_inter$region_name))
+    # dt_aggr <- rep(list(c(met = 0, demet = 0)), length(keys))
+    # dt_aggr <- setNames(dt_aggr, keys)
  
-    message("!!! loop ", nrow(dt_inter))
-    for (i in 1:nrow(dt_inter)) {
-      if (i %% 25000 == 0) {
-        message("!!! row ", i)
-      }
-      region_name <- dt_inter[i, "region_name"]
-      met <- dt_inter[i, "met"]
-      demet <- dt_inter[i, "demet"]
+    # message("!!! loop ", nrow(dt_inter))
+    # for (i in 1:nrow(dt_inter)) {
+    #   if (i %% 25000 == 0) {
+    #     message("!!! row ", i)
+    #   }
+    #   region_name <- dt_inter[i, "region_name"]
+    #   met <- dt_inter[i, "met"]
+    #   demet <- dt_inter[i, "demet"]
 
-      dt_aggr[[region_name]]["met"] <- dt_aggr[[region_name]]["met"] + met
-      dt_aggr[[region_name]]["demet"] <- dt_aggr[[region_name]]["demet"] + demet
-    }
+    #   dt_aggr[[region_name]]["met"] <- dt_aggr[[region_name]]["met"] + met
+    #   dt_aggr[[region_name]]["demet"] <- dt_aggr[[region_name]]["demet"] + demet
+    # }
 
-    dt_aggr <- as.data.frame(do.call(rbind, dt_aggr))
-    dt_aggr$region_name <- rownames(dt_aggr)
+    # dt_aggr <- as.data.frame(do.call(rbind, dt_aggr))
+    # dt_aggr$region_name <- rownames(dt_aggr)
 
     # lst <- tapply(dt_inter$region_name, 1:length(dt_inter$region_name), c)
     # for (k in names(lst)) {
@@ -212,6 +211,7 @@ compute_call_count_matrices <- function(  df_region,
     #   dt_aggr <- rbind(dt_aggr, row)
     # }
   
+    dt_aggr <- dt_inter[, vapply(.SD, sum, numeric(1)), by = "region_name", .SDcols = c("met", "demet") ]
     # dt_aggr <- dt_inter[, unlist(lapply(.SD, sum)), by = .(region_name), .SDcols = quant_cols ]
   
     dt_aggr$call_count = dt_aggr$met + dt_aggr$demet
